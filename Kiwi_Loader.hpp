f58537141ba540c97ba7e945a326ba2c354169cb
Pierre <guillotpierre6@gmail.com>
2018-04-04 15:45:36 +0200

    bim

--- Kiwi_Loader.hpp
+++ Kiwi_Loader.hpp
@@ -30,7 +30,7 @@ namespace kiwi
             
             static Object* create(std::string const& path, std::string const& name)
             {
-                const std::string fullpath = path.empty() ? name + ".kiwix" : path + "/" + name + ".kiwix";
+                const std::string fullpath = path.empty() ? name + ".kiwix" : path + std::string(s_separator) + name + ".kiwix";
                 auto& olib = get().libs[fullpath];
                 if(!olib.lib)
                 {
@@ -134,8 +134,10 @@ namespace kiwi
             
 #ifdef _WIN32
             typedef HINSTANCE lib_t;
+            constexpr static char const* const s_separator = "\";
 #else
             typedef void* lib_t;
+            constexpr static char const* const s_separator = "/";
 #endif
             struct object_lib
             {
